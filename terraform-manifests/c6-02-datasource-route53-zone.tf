44640743fd502036c9b6c9e3e6c9bf35c23649e8
rajdev0ps <raj.deolekar.ed@outlook.com>
2023-02-10 13:59:27 +0530

    route53 domainname change c6-02

--- terraform-manifests/c6-02-datasource-route53-zone.tf
+++ terraform-manifests/c6-02-datasource-route53-zone.tf
@@ -1,6 +1,6 @@
 # Get DNS information from AWS Route53
 data "aws_route53_zone" "mydomain" {
-  name         = "devopsincloud.com"
+  name         = "devsecopscloud.click"
 }
 
 # Output MyDomain Zone ID
